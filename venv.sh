5d00c8e38aa86fdcd36f875d369ad0c5ecad5995
1basilisk <1basilisk@github.com>
2023-03-09 19:01:57 +0530

    updated venv script

--- venv.sh
+++ venv.sh
@@ -1,4 +1,4 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 
 sudo apt install python3.8-distutils -y
 sudo apt install virtualenv
@@ -6,5 +6,6 @@ sudo apt install virtualenv
 virtualenv -p python3.8 env
 
 source env/bin/activate
+pip install --requirement requirements.txt
 
 echo "to run the app: flask run"
