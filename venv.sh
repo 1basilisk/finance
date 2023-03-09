7685bcfd988a6ac053745c95d2be347ea47e95d6
1basilisk <1basilisk@github.com>
2023-03-09 23:27:07 +0530

    updated installer

--- venv.sh
+++ venv.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-
-sudo apt install python3.8-distutils -y
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.8
 sudo apt install virtualenv
-
 virtualenv -p python3.8 env
-
 source env/bin/activate
 pip install --requirement requirements.txt
 
